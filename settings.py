628a63104ac71eec4840eb79272849a514f5bd40
Steenvoort <P70071686@unimaas.nl>
2021-07-09 09:49:45 +0200

    Modified runtime.txt, requirements.txt and settings.py

--- settings.py
+++ settings.py
@@ -39,3 +39,6 @@ ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 DEMO_PAGE_INTRO_HTML = """ """
 
 SECRET_KEY = '2133226331992'
+
+# if an app is included in SESSION_CONFIGS, you don't need to list it here
+INSTALLED_APPS = ['otree']
